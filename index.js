78f9324ffef24db0d365a3ed6a046da95eebbf9b
C3NZ <cenz@cenz.io>
2019-07-16 11:05:19 -0700

    [feat] add round, floor, and ceil functions

--- index.js
+++ index.js
@@ -3,3 +3,32 @@
  * @type {Number}
  */
 Number.GOLDENRATIO = (1 + Math.sqrt(5)) / 2;
+
+/**
+ * Round a number to it's nearest whole number
+ * @function
+ * @returns {Number} the closest whole number to the original number
+ */
+Number.prototype.round = function() {
+  return Math.round(this);
+};
+
+/**
+ * Round a number down to it's nearest whole number
+ * @function
+ * @returns {Number} the closest whole number to the rounded down original number
+ */
+Number.prototype.floor() = function() {
+  return Math.floor(this);
+}
+
+/**
+ * Round a number to the highest 
+ * @function
+ * @returns {Number} the closest whole number to the rounded up original number
+ */
+Number.prototype.ceil() = function() {
+  return Math.ceil(this);
+}
+
+console.log(Number(4.3).round());
