4c0979ebd71cb2c906d3dd596fad7b0bbeeb32dd
C3NZ <cenz@cenz.io>
2019-07-18 14:15:50 -0700

    [feat] successfully split up the numbers for the padding.

--- index.js
+++ index.js
@@ -26,7 +26,31 @@ Number.prototype.floor = function floor() {
  * Round a number to the highest
  * @function
  * @returns {Number} the closest whole number to the rounded up original number
+ * @examples
+ *  Number(5.32).ceil() -> 6
  */
 Number.prototype.ceil = function ceil() {
   return Math.ceil(this);
 };
+
+/**
+ * Pad a number with a certain amount of zeros
+ * @function
+ * @returns {String} A padded string that indicates
+ */
+Number.prototype.pad = function pad(leftPadding, rightPadding) {
+  const numAsArray = String(this).split(".");
+  // If the number has a decimal place...
+
+  if (numAsArray.length > 1) {
+    const leftSide = numAsArray[0];
+    const rightSide = numAsArray[1];
+    console.log(leftSide, rightSide);
+  } else {
+    const num = numAsArray[0];
+    console.log(num);
+  }
+};
+
+console.log(Number(34.43).pad(0, 0));
+console.log(Number("3453").pad(0, 0));
