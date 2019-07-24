6050d3df5131b7ede618d07cf8e6e727547c2f4e
C3NZ <cenz@cenz.io>
2019-07-23 19:58:58 -0700

    [add] rollup js for packaging the index.js and husky for a git hooks

---
+++ rollup.config.js
@@ -0,0 +1,21 @@
+import { terser } from "rollup-plugin-terser";
+
+export default [
+  {
+    input: "index.js",
+    plugins: [terser()],
+    output: {
+      file: "umd/index.js",
+      format: "umd",
+      name: "mathModule",
+      esModule: false,
+    },
+  },
+  {
+    input: "index.js",
+    output: {
+      file: "esm/index.js",
+      format: "esm",
+    },
+  },
+];
