e1dad9b28d4e8a6078694cc32883b390c7f87eb5
C3NZ <cenz@cenz.io>
2019-08-08 15:16:43 -0700

    [add] bundled output

--- esm/index.js
+++ esm/index.js
@@ -2,23 +2,23 @@
  * @desc The golden ratio as approximated by: https://en.wikipedia.org/wiki/Golden_ratio
  * @type {Number}
  */
-Number.GOLDENRATIO = (1 + Math.sqrt(5)) / 2;
+var GOLDENRATIO = (1 + Math.sqrt(5)) / 2;
 /**
  * @function
  * @desc Round a number to it's nearest whole number
  * @return {Number} the closest whole number to the original number
  */
-Number.prototype.round = function round() {
-    return Math.round(this);
-};
+function round(num) {
+    return Math.round(num);
+}
 /**
  * @function
  * @desc Round a number down to it's nearest whole number.
  * @return {Number} the closest whole number to the rounded down original number.
  */
-Number.prototype.floor = function floor() {
-    return Math.floor(this);
-};
+function floor(num) {
+    return Math.floor(num);
+}
 /**
  * @function
  * @desc Round a number up to it's closest whole number
@@ -26,9 +26,9 @@ Number.prototype.floor = function floor() {
  * @example
  * Number(5.32).ceil() -> 6
  */
-Number.prototype.ceil = function ceil() {
-    return Math.ceil(this);
-};
+function ceil(num) {
+    return Math.ceil(num);
+}
 /**
  * @function
  * @desc Pad a number with a specified amount of zeros (On either the left or right side)
@@ -38,8 +38,8 @@ Number.prototype.ceil = function ceil() {
  * Number(4.34).pad(2,0) -> 04.34
  * Number(23.32).pad(10,10) -> 0000000023.3200000000
  */
-Number.prototype.pad = function pad(leftPadding, rightPadding) {
-    var numAsArray = String(this).split(".");
+function pad(num, leftPadding, rightPadding) {
+    var numAsArray = String(num).split(".");
     // If the number has a decimal place...
     if (numAsArray.length > 1) {
         // Get the left side of the number
@@ -63,10 +63,10 @@ Number.prototype.pad = function pad(leftPadding, rightPadding) {
     }
     // There isn't a decimal place within the number,
     // no need to add right padding.
-    var num = numAsArray[0];
-    var paddingNeeded = leftPadding - num.length;
+    var outputNum = numAsArray[0];
+    var paddingNeeded = leftPadding - outputNum.length;
     return "0".repeat(paddingNeeded) + num;
-};
+}
 /**
  * @function
  * @desc Convert a number from degrees to radians
@@ -74,9 +74,9 @@ Number.prototype.pad = function pad(leftPadding, rightPadding) {
  * @example
  * Number(20).degToRad() -> 0.349066
  */
-Number.prototype.degToRad = function degToRad() {
-    return this * (Math.PI / 180);
-};
+function degToRad(num) {
+    return num * (Math.PI / 180);
+}
 /**
  * @function
  * @desc Convert a number from radians to degrees
@@ -84,9 +84,9 @@ Number.prototype.degToRad = function degToRad() {
  * @example
  * Number(1).radToDeg() -> 57.2958
  */
-Number.prototype.radToDeg = function radToDeg() {
-    return this / (Math.PI / 180);
-};
+function radToDeg(num) {
+    return num / (Math.PI / 180);
+}
 /**
  * @function
  * @desc Convert a number to USD.
@@ -96,8 +96,8 @@ Number.prototype.radToDeg = function radToDeg() {
  * Number(1.05).toDollars() -> $1.05
  * Number(0.5).toDollars() -> ¢0.5
  */
-Number.prototype.toDollars = function toDollars() {
-    var numArr = String(this).split(".");
+function toDollars(num) {
+    var numArr = String(num).split(".");
     // Number is a whole number
     if (numArr.length === 1) {
         return "$" + numArr[0] + ".00";
@@ -108,7 +108,7 @@ Number.prototype.toDollars = function toDollars() {
     }
     // Number is fractional and greater than 0
     return "$" + numArr[0] + "." + numArr[1];
-};
+}
 /**
  * @function
  * @desc Return the tax amount from the number that would be applied given the rate
@@ -119,17 +119,17 @@ Number.prototype.toDollars = function toDollars() {
  * Number(10).tax(10) -> 1.00
  * Number(60).tax(25) -> 15.00
  */
-Number.prototype.tax = function tax(rate) {
+function tax(num, rate) {
     // Anything times zero is zero
     if (rate === 0) {
         return 0;
     }
     // Bound checking for the rate
     if (rate >= 0 && rate <= 100) {
-        return this * (rate / 100);
+        return num * (rate / 100);
     }
     throw new Error("The tax rate needs to be within 0 and 1! or 0 and 100!");
-};
+}
 /**
  * @function
  * @desc Return this number with a specified tax rate applied to it.
@@ -139,9 +139,9 @@ Number.prototype.tax = function tax(rate) {
  * Number(10).withTax(10) -> 11
  * Number(200).withTax(50) -> 300
  */
-Number.prototype.withTax = function withTax(rate) {
-    return this + this.tax(rate);
-};
+function withTax(num, rate) {
+    return num + tax(num, rate);
+}
 /**
  * @function
  * @desc Calculate the interest on a number given the interest rate, years, and optional
@@ -153,7 +153,7 @@ Number.prototype.withTax = function withTax(rate) {
  * Number(10).interest(1, 10) -> "110.00"
  * Number(2).interest(80, 50) -> "82.00"
  */
-Number.prototype.interest = function interest(rate, years, decimalPlaces) {
+function interest(num, rate, years, decimalPlaces) {
     if (decimalPlaces === void 0) { decimalPlaces = 2; }
     if (rate < 0) {
         throw new Error("You cannot enter a interest rate lower than 0%!");
@@ -162,8 +162,8 @@ Number.prototype.interest = function interest(rate, years, decimalPlaces) {
         throw new Error("You cannot go back in time! Enter a year greater than 0!");
     }
     var interestRate = rate / 100;
-    return (this * (1 + interestRate * years)).toFixed(decimalPlaces);
-};
+    return (num * (1 + interestRate * years)).toFixed(decimalPlaces);
+}
 /**
  * @function
  * @desc Calculate the monthly payments for a mortage given a principal (Number)
@@ -174,7 +174,7 @@ Number.prototype.interest = function interest(rate, years, decimalPlaces) {
  * Number(100000).mortage(3.92, 30) -> (Roughly) 473
  * Number(50000).mortage(5.0, 10) -> (Roughly) 530
  */
-Number.prototype.mortage = function mortage(interestRate, numberOfYears) {
+function mortage(num, interestRate, numberOfYears) {
     if (numberOfYears < 0) {
         throw new Error("You cannot have a negative amount of years to make payments!");
     }
@@ -187,8 +187,8 @@ Number.prototype.mortage = function mortage(interestRate, numberOfYears) {
     var numerator = Math.pow(1 + monthlyInterest, totalPayments);
     // Bottom half of the mortage equation
     var denominator = Math.pow(1 + monthlyInterest, totalPayments) - 1;
-    return (this * monthlyInterest * numerator) / denominator;
-};
+    return (num * monthlyInterest * numerator) / denominator;
+}
 /**
  * @function
  * @desc convert a decimal number to hexadecimal, all thanks to the help of this
@@ -198,12 +198,29 @@ Number.prototype.mortage = function mortage(interestRate, numberOfYears) {
  * Number(0).decimalToHex() -> "0"
  * Number(15).decimalToHex() -> "0xF"
  */
-Number.prototype.decimalToHex = function decimalToHex() {
-    var num = this;
+function decimalToHex(num) {
     // If the number is less than 0
+    var internalNum = num;
     if (num < 0) {
-        num += 0xffffffff + 1;
+        internalNum += 0xffffffff + 1;
     }
-    return "0x" + num.toString(16).toUpperCase();
-};
+    return "0x" + internalNum.toString(16).toUpperCase();
+}
 module.exports.random = function (maxNum) { return Math.floor(Math.random() * maxNum); };
+var index = {
+    pad: pad,
+    round: round,
+    floor: floor,
+    ceil: ceil,
+    degToRad: degToRad,
+    radToDeg: radToDeg,
+    toDollars: toDollars,
+    tax: tax,
+    withTax: withTax,
+    interest: interest,
+    mortage: mortage,
+    decimalToHex: decimalToHex,
+};
+
+export default index;
+export { GOLDENRATIO, ceil, decimalToHex, degToRad, floor, interest, mortage, pad, radToDeg, round, tax, toDollars, withTax };
--- umd/index.d.ts
+++ umd/index.d.ts
@@ -1,17 +1,130 @@
-interface NumberConstructor {
-    GOLDENRATIO: number;
-}
-interface Number {
-    round(): number;
-    floor(): number;
-    ceil(): number;
-    pad(leftPadding: number, rightPadding: number): string;
-    degToRad(): number;
-    radToDeg(): number;
-    toDollars(): string;
-    tax(rate: number): number;
-    withTax(rate: number): number;
-    interest(rate: number, years: number, decimalPlaces?: number): string;
-    mortage(interestRate: number, years: number): number;
-    decimalToHex(): string;
-}
+/**
+ * @desc The golden ratio as approximated by: https://en.wikipedia.org/wiki/Golden_ratio
+ * @type {Number}
+ */
+export declare const GOLDENRATIO: number;
+/**
+ * @function
+ * @desc Round a number to it's nearest whole number
+ * @return {Number} the closest whole number to the original number
+ */
+export declare function round(num: number): number;
+/**
+ * @function
+ * @desc Round a number down to it's nearest whole number.
+ * @return {Number} the closest whole number to the rounded down original number.
+ */
+export declare function floor(num: number): number;
+/**
+ * @function
+ * @desc Round a number up to it's closest whole number
+ * @return {Number} the closest whole number to the rounded up original number.
+ * @example
+ * Number(5.32).ceil() -> 6
+ */
+export declare function ceil(num: number): number;
+/**
+ * @function
+ * @desc Pad a number with a specified amount of zeros (On either the left or right side)
+ * @return {String} A padded string that indicates.
+ * @example
+ * Number(5.3234).pad(4,5) -> 0004.32340
+ * Number(4.34).pad(2,0) -> 04.34
+ * Number(23.32).pad(10,10) -> 0000000023.3200000000
+ */
+export declare function pad(num: number, leftPadding: number, rightPadding: number): string;
+/**
+ * @function
+ * @desc Convert a number from degrees to radians
+ * @return {Number} The converted number in radians
+ * @example
+ * Number(20).degToRad() -> 0.349066
+ */
+export declare function degToRad(num: number): number;
+/**
+ * @function
+ * @desc Convert a number from radians to degrees
+ * @return {Number} the converted number in degrees.
+ * @example
+ * Number(1).radToDeg() -> 57.2958
+ */
+export declare function radToDeg(num: number): number;
+/**
+ * @function
+ * @desc Convert a number to USD.
+ * @return {String} The dollar representation of the number
+ * @example
+ * Number(1).toDollars() -> $1.00
+ * Number(1.05).toDollars() -> $1.05
+ * Number(0.5).toDollars() -> ¢0.5
+ */
+export declare function toDollars(num: number): string;
+/**
+ * @function
+ * @desc Return the tax amount from the number that would be applied given the rate
+ * @param {Number} rate - The tax rate to be applied where the rate is 0 <= rate <= 100
+ * or the rate is 0 <= 100
+ * @return {Number} the total tax amount of the current number multiplied by the rate
+ * @example
+ * Number(10).tax(10) -> 1.00
+ * Number(60).tax(25) -> 15.00
+ */
+export declare function tax(num: number, rate: number): number;
+/**
+ * @function
+ * @desc Return this number with a specified tax rate applied to it.
+ * @param {Number} rate - The tax rate to be applied
+ * @return {Number} a new number that has the tax of the original number applied.
+ * @example
+ * Number(10).withTax(10) -> 11
+ * Number(200).withTax(50) -> 300
+ */
+export declare function withTax(num: number, rate: number): number;
+/**
+ * @function
+ * @desc Calculate the interest on a number given the interest rate, years, and optional
+ * decimal places to round to
+ * @param {Number} rate - The interest rate
+ * @param {Number} years - The amount of years the rate is applied to
+ * @return {String} a string containing the total interest paid on a given number
+ * @example
+ * Number(10).interest(1, 10) -> "110.00"
+ * Number(2).interest(80, 50) -> "82.00"
+ */
+export declare function interest(num: number, rate: number, years: number, decimalPlaces?: number): string;
+/**
+ * @function
+ * @desc Calculate the monthly payments for a mortage given a principal (Number)
+ * @param {Number} numberOfYears - the years remaining for the mortage
+ * @param {Number} interestRate - The yearly rate of interest
+ * @return {Number} The monthly mortage payments that need to be made to pay off the mortage
+ * @example
+ * Number(100000).mortage(3.92, 30) -> (Roughly) 473
+ * Number(50000).mortage(5.0, 10) -> (Roughly) 530
+ */
+export declare function mortage(num: number, interestRate: number, numberOfYears: number): number;
+/**
+ * @function
+ * @desc convert a decimal number to hexadecimal, all thanks to the help of this
+ * stack overflow question: https://stackoverflow.com/questions/57803/how-to-convert-decimal-to-hexadecimal-in-javascript
+ * @return {String} The string containing the number converted to hexadecimal
+ * @example
+ * Number(0).decimalToHex() -> "0"
+ * Number(15).decimalToHex() -> "0xF"
+ */
+export declare function decimalToHex(num: number): string;
+declare const _default: {
+    pad: typeof pad;
+    round: typeof round;
+    floor: typeof floor;
+    ceil: typeof ceil;
+    degToRad: typeof degToRad;
+    radToDeg: typeof radToDeg;
+    toDollars: typeof toDollars;
+    tax: typeof tax;
+    withTax: typeof withTax;
+    interest: typeof interest;
+    mortage: typeof mortage;
+    decimalToHex: typeof decimalToHex;
+};
+export default _default;
